489082d4e06fa6c6dca599da506ba3c21e0d5f04
Luis Garcia <roci@adakailabs.com>
2021-07-26 19:54:20 -0600

    add traceroute

--- cardanocfg/configfiles_test.go
+++ cardanocfg/configfiles_test.go
@@ -1,12 +1,16 @@
 package cardanocfg_test
 
 import (
+	"fmt"
+	"log"
+	"net"
 	"os"
 	"testing"
 	"time"
 
-	"github.com/adakailabs/gocnode/fastping"
+	"github.com/adakailabs/go-traceroute/traceroute"
 
+	"github.com/adakailabs/gocnode/fastping"
 	"github.com/k0kubun/pp"
 
 	"github.com/adakailabs/gocnode/cardanocfg"
@@ -16,7 +20,8 @@ import (
 	"github.com/adakailabs/gocnode/config"
 )
 
-const cfgFile = "/home/galuisal/Documents/cardano/adakailabs/cardano-docker/cardano-node/gocnode/gocnode.yaml"
+// const cfgFile = "/home/galuisal/Documents/cardano/adakailabs/cardano-docker/cardano-node/gocnode/gocnode.yaml"
+const cfgFile = "/home/galuisal/Documents/cardano/cardano-docker/cardano-node/gocnode/gocnode.yaml"
 
 func TestMain(m *testing.M) {
 	// call flag.Parse() here if TestMain uses flags
@@ -42,7 +47,6 @@ func TestConfig(t *testing.T) {
 
 func TestConfigTopology(t *testing.T) {
 	a := assert.New(t)
-	//const cfgFile = "/home/galuisal/Documents/cardano/adakailabs/cardano-docker/cardano-node/stack/secrets/gocnode.yaml"
 
 	c, err := config.New(cfgFile, true, "Debug")
 	a.Nil(err)
@@ -118,7 +122,7 @@ func TestConfigTestnetOptimzer(t *testing.T) {
 		t.FailNow()
 	}
 
-	relays, err4 := d.TestLatencyWithPing(relays)
+	_, relays, err4 := d.TestLatencyWithPing(relays)
 	if !a.Nil(err4) {
 		t.FailNow()
 	}
@@ -126,13 +130,64 @@ func TestConfigTestnetOptimzer(t *testing.T) {
 	pp.Print(relays)
 
 	for _, p := range relays {
-		pp.Printf("relay: %s --> %s \n", p.Addr, p.Latency.Milliseconds())
+		pp.Printf("relay: %s --> %s \n", p.Addr, p.GetLatency().Milliseconds())
+	}
+}
+
+func TestValency(t *testing.T) {
+	a := assert.New(t)
+
+	c, err := config.New(cfgFile, true, "Debug")
+	if !a.Nil(err) {
+		t.FailNow()
+	}
+
+	d, err2 := cardanocfg.New(&c.Relays[0], c)
+	if !a.Nil(err2) {
+		t.FailNow()
+	}
+
+	tAddr := "relays-new.cardano-testnet.iohkdev.io"
+
+	ips, err := net.LookupIP(tAddr)
+	if !a.Nil(err) {
+		t.FailNow()
+	}
+
+	if !a.Equal(8, len(ips)) {
+		t.FailNow()
+	}
+
+	fmt.Println(ips)
+
+	addr := net.ParseIP("relays-new.cardano-testnet.iohkdev.io")
+	if !a.Nil(addr) {
+		t.FailNow()
+	}
+
+	relays := make(cardanocfg.NodeList, 1)
+	relays[0].Addr = tAddr
+
+	for _, addr := range ips {
+		addrIP := net.ParseIP(addr.String())
+		if !a.NotNil(addrIP) {
+			t.FailNow()
+		}
+	}
+
+	relays, err = d.SetValency(relays)
+	if err != nil {
+		t.FailNow()
+	}
+
+	if !a.Equal(uint(8), relays[0].Valency) {
+		t.FailNow()
 	}
 }
 
 func TestPinger(t *testing.T) {
 	a := assert.New(t)
-	pTime, err := fastping.TestAddress("www.google.com")
+	pTime, _, err := fastping.TestAddress("www.google.com")
 
 	if !a.Nil(err) {
 		t.FailNow()
@@ -143,7 +198,6 @@ func TestPinger(t *testing.T) {
 
 func TestConfigDownloadAndSetTopology(t *testing.T) {
 	a := assert.New(t)
-	const cfgFile = "/home/galuisal/Documents/cardano/adakailabs/gocnode/gocnode.yaml"
 
 	c, err := config.New(cfgFile, true, "Debug")
 	a.Nil(err)
@@ -173,6 +227,61 @@ func TestConfigMainnetTopology(t *testing.T) {
 	pp.Print(relays)
 }
 
+func TestTraceRouteGoogle(t *testing.T) {
+	//a := assert.New(tr)
+	hosts, _ := net.LookupIP("www.google.com")
+	ip := hosts[0]
+	hops, err := traceroute.Trace(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, h := range hops {
+		for _, n := range h.Nodes {
+			log.Printf("%d. %v %v", h.Distance, n.IP, n.RTT)
+		}
+	}
+}
+
+//
+//func TestTraceRoute(t *testing.T) {
+//	//host := "relays-new.cardano-testnet.iohkdev.io"
+//	a := assert.New(t)
+//
+//	c, err := config.New(cfgFile, true, "Debug")
+//	if !a.Nil(err) {
+//		t.FailNow()
+//	}
+//
+//	d, err2 := cardanocfg.New(&c.Relays[0], c)
+//	if !a.Nil(err2) {
+//		t.FailNow()
+//	}
+//
+//	_, relays, err3 := d.GetTestNetRelays()
+//	if !a.Nil(err3) {
+//		t.FailNow()
+//	}
+//
+//	for _, relay := range relays {
+//		host := relay.Addr
+//		fmt.Println("testing relay: ", host)
+//		hostIP, _ := net.LookupHost(host)
+//		fmt.Println("hostIP: ", hostIP)
+//		hops, err := traceroute.Trace(net.ParseIP(hostIP[0]))
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//		for _, h := range hops {
+//			for _, n := range h.Nodes {
+//				log.Printf("%d. %v %v", h.Distance, n.IP, n.RTT)
+//			}
+//		}
+//
+//		fmt.Println("hop count: ", len(hops))
+//	}
+//}
+
+/*
 func TestConfigMainnetTopologyStream(t *testing.T) {
 	a := assert.New(t)
 	const cfgFile = "/home/galuisal/Documents/cardano/adakailabs/gocnode/gocnode.yaml"
@@ -206,3 +315,4 @@ func TestConfigMainnetTopologyStream(t *testing.T) {
 
 	t.Log("end")
 }
+*/
